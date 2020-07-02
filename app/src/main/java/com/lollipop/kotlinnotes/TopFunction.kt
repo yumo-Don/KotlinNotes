ea5b7fd01e3ae11e3a4223ce52e12e9feecad713
lollipop <976731597@qq.com>
2020-07-02 10:48:37 +0800

    kotlin 扩展函数 注释

--- app/src/main/java/com/lollipop/kotlinnotes/TopFunction.kt
+++ app/src/main/java/com/lollipop/kotlinnotes/TopFunction.kt
@@ -5,7 +5,7 @@ package com.lollipop.kotlinnotes
 /**
  * @Author: lollipop
  * @Document: 顶层函数，顶层属性
- * 顶层函数编译生成的类名，对应包含它的文件的名字，
+ * 顶层函数编译生成的类名，对应包含它的文件的名字，eg:当前类编译后的类名就是TopFunction（如果没用使用jvmName注解的话）
  * 这个文件中的所有顶层函数都会编译为这个类的静态函数
  * eg：topOne（）编译生成的类，就是TopFunctionKT
  * 如果想修改这个类的名字，可以使用@file:JvmName注解，需注意此注解需要放在包名的前面
@@ -22,7 +22,7 @@ fun topOne() {
 }
 
 /**
- * 扩展函数
+ * 扩展函数,不能访问私有的或者受保护的成员
  */
 fun String.firstChar(): Char {
     return this[0]
